f8f620c86f36e03eeda919c6a2b322b74af8edfa
Jamil <dreamsllcometrue@gmail.com>
2017-06-23 13:38:39 +0100

    Added filter function after splitting the string

--- word-counter.js
+++ word-counter.js
@@ -12,10 +12,20 @@ var txtBox = document.getElementById("txtBox");
 
 function countWords (e) {
   if ( e.keyCode === 32 || e.keyCode === 8 ) {
-    var str = txtBox.value;
-    var words = str.split(" ").filter(function(word){ if ( word !== ""){ return word;} });
-    numWords.value = words.length;
-    console.log( words.length );
+      var str = txtBox.value;
+      if ( str.length <= 0 ) {
+          numWords.value = 0;
+      }
+      else {
+         var words = str.split(/\s+/g);
+         words = words.filter(function(word){
+             if ( word !== "undefined" ) {
+                 return word;
+             }
+         });  
+         numWords.value = words.length;
+      }
+   
   }
 }
 
