983817b0057637d806a0c134aefa340befe71136
Jamil <dreamsllcometrue@gmail.com>
2017-06-23 16:51:52 +0100

    Finally made the program working as I wanted, it now shows time elapsed, how many words typed and wpm speed

--- word-counter.js
+++ word-counter.js
@@ -2,6 +2,19 @@ var numWords = document.getElementById("output");
 var txtBox = document.getElementById("txtBox");
 var timePassed = document.querySelector(".timePassed");
 var wpmBox = document.querySelector(".wordPM");
+var counter = 0;
+
+function startTimer() {
+     if ( txtBox.value.length > 0 ) {
+          counter++;
+          timePassed.innerHTML = "Time Elapsed : " + counter;
+     }
+
+}
+
+    setInterval(startTimer, 1000);
+
+
 function countWords (e) {
   
       var str = txtBox.value;
@@ -9,6 +22,8 @@ function countWords (e) {
           numWords.value = 0;
       }
       else {
+         
+        
          var words = str.split(" ");
          words = words.filter(function(word){
              if ( word !== "undefined" ) {
@@ -16,11 +31,10 @@ function countWords (e) {
              }
          });  
          numWords.value = words.length;
+         wpmBox.innerHTML = "WPM : " + Math.floor((words.length)/(counter/60));
       }
-   
+        
   
 }
 
 txtBox.addEventListener("keyup" , countWords);
-
-wpmBox.addEventListener("click" , function(){alert("Hello");});
